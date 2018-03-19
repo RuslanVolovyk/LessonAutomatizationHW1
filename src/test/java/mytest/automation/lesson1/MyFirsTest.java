24d6523086f5dae51e30d21c136d4fe62337fedb
Ruslan <ecologist4@gmail.com>
2018-03-19 17:42:39 +0200

    add cicle

--- src/test/java/mytest/automation/lesson1/MyFirsTest.java
+++ src/test/java/mytest/automation/lesson1/MyFirsTest.java
@@ -7,7 +7,27 @@ import java.io.File;
 
 public class MyFirsTest {
     public static void main(String[] args) {
+        methodCicleFor();
+        methodCicleWhile();
+//        initDraver();
+    }
 
+    public static void methodCicleFor() {
+        int i = 0;
+        for (i = 0; i < 100; i++) {
+            System.out.println("Cicle for " + i);
+        }
+    }
+
+    public static void methodCicleWhile() {
+        int i = 100;
+        while (i > 0) {
+            System.out.println("Cicle while " + i);
+            i--;
+        }
+    }
+
+    public static void initDraver() {
         System.setProperty("webdriver.chrome.driver",
                 new File(MyFirsTest.class.getResource("/chromedriver.exe").getFile()).getPath());
 
